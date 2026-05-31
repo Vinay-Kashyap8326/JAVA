9c9be5c7498777a3a611e6bc5d2f0085d7dc6281
Vinay-Kashyap8326 <vinay2911kashyap@gmail.com>
2026-05-31 14:55:05 +0530

    Insertion Sort

--- Sorting/Insertion_sort.java
+++ Sorting/Insertion_sort.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class Insertion_sort {
   public static void main(String[] args) {
-    int arr[] = {5,4,3,2,1};
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Length Of Array: ");
+    int size = sc.nextInt();
+    int arr[] = new int [size];
+    System.out.print("Enter Elements Of Array: ");
+    for(int i = 0; i < size; i++){
+      arr[i] = sc.nextInt();
+    }
     for(int i = 1; i < arr.length; i++){
       int current = arr[i];
       int j = i-1;
@@ -8,6 +16,9 @@ public class Insertion_sort {
         arr[j+1] = arr[j];
       }
       arr[j+1] = current;
+    }
+    for(int num : arr){
+      System.out.print(num + " ");
     }
   }
 }
